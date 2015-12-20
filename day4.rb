420f2f94833ab4113871c31b0811b90c948d034a
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-20 13:49:39 -0800

    second half of day 4 complete

--- day4.rb
+++ day4.rb
@@ -12,4 +12,16 @@ def advent_coins(input)
   return string
 end
 
-puts "The code is #{advent_coins(INPUT)}"
+def advent_coins_2(input)
+  # begin the string with a number
+  string = "1"
+  # use the Ruby MD5 digest-- I don't really understand this, but it does it's thing until the first five digits are 0
+  while Digest::MD5.hexdigest("#{input}" + string)[0..5] != "000000"
+    # increment the numbers by 1, so the first one it finds that fits the requirements will be the lowest
+    string = string.succ
+  end
+  return string
+end
+
+puts "For five zeros, the code is #{advent_coins(INPUT)}"
+puts "For six zeros, the code is #{advent_coins_2(INPUT)}"
