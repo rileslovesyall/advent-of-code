3dcaee4ef8a3e60c2f176e11bc3a60d3200186bd
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-20 12:24:26 -0800

    day 4 part one solved

---
+++ day4.rb
@@ -0,0 +1,15 @@
+require "./inputs/day4_input.rb"
+require "digest"
+
+def advent_coins(input)
+  # begin the string with a number
+  string = "1"
+  # use the Ruby MD5 digest-- I don't really understand this, but it does it's thing until the first five digits are 0
+  while Digest::MD5.hexdigest("#{input}" + string)[0..4] != "00000"
+    # increment the numbers by 1, so the first one it finds that fits the requirements will be the lowest
+    string = string.succ
+  end
+  return string
+end
+
+puts "The code is #{advent_coins(INPUT)}"
