2a2ad4155713e8bddd8c6fa01e6a31484d717766
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-20 11:05:41 -0800

    first half of day 3 figured out

--- day3.rb
+++ day3.rb
@@ -1,7 +1,24 @@
 require "./inputs/day3_input.rb"
 
 def houses__with_presents(radio_directions)
-  house_map = [[]]
+  house_map = radio_directions.split("")
+  visited_houses = [[0,0]]
+  # starting coordinates
+  x = 0
+  y = 0
+  house_map.each do |direction|
+    if direction == "^"
+      y += 1
+    elsif direction == "v"
+      y -= 1
+    elsif direction == "<"
+      x -= 1
+    elsif direction == ">"
+      x += 1
+    end
+    visited_houses << [x, y]
+  end
+  return visited_houses.uniq.length
 end
 
 test = ">"
