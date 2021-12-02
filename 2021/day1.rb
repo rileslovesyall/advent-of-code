b4069e968ad682610e2611bbc682ad4979a929bc
Riley <riley.r.spicer@gmail.com>
2021-12-01 20:58:01 -0800

    Solved Day 1

--- 2021/day1.rb
+++ 2021/day1.rb
@@ -1,2 +1,16 @@
-relative_require './inputs/day1_inputs.rb'
+def count_distance(input)
+  last_line = 0
+  greater = 0
+  lesser = 0
 
+  File.readlines(input).each do |line|
+    last_line = line if last_line == 0 
+    greater+= 1 if last_line.to_i < line.to_i
+    last_line = line
+  end
+
+  puts "Increased: #{greater}"
+
+end
+
+count_distance('./inputs/day1_inputs.txt')
