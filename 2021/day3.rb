2636a4ddc421b0170f81522be97e70dd72dfb252
Riley <riley.r.spicer@gmail.com>
2021-12-05 20:47:32 -0800

    Completed Day 3

--- 2021/day3.rb
+++ 2021/day3.rb
@@ -82,6 +82,119 @@ def get_median(array)
     median = (sorted[(len-1)/2].to_i + sorted[len/2].to_i / 2)
 end
 
+def get_most(array)
+	zero_count = 0
+	one_count = 0
+	len = array.length
+
+	while zero_count <= array.length && one_count <= array.length
+		array.each do |n|
+			if n == "0"
+				zero_count += 1
+			else
+				one_count += 1
+			end
+		end
+	end
+
+	if zero_count > one_count
+		return "0"
+	else
+		return "1"
+	end
+end
+
+def get_least(array)
+	zero_count = 0
+	one_count = 0
+	len = array.length
+
+	while zero_count <= array.length && one_count <= array.length
+		array.each do |n|
+			if n == "0"
+				zero_count += 1
+			else
+				one_count += 1
+			end
+		end
+	end
+
+	if one_count < zero_count
+		return "1"
+	else
+		return "0"
+	end
+end
+
+def get_life_support(input)
+	input_array = []
+	File.readlines(input).each do |line|
+		input_array << line.chomp
+	end
+	puts "Working on life support."
+
+	ox_gen_rating_binary = get_oxygen_generator_rating(input_array)
+	ox_gen_rating_dec = ox_gen_rating_binary.to_i(2)
+	puts "Ox Gen Rating = #{ox_gen_rating_dec} or #{ox_gen_rating_binary}"
+
+	co2_scrubber_rating_binary = get_co2_scrubber_rating(input_array)
+	co2_scrubber_rating_dec = co2_scrubber_rating_binary.to_i(2)
+	puts "CO2 Scrubber Rating = #{co2_scrubber_rating_dec} or #{co2_scrubber_rating_binary}"
+
+	life_support_rating = ox_gen_rating_dec * co2_scrubber_rating_dec
+	puts "Life suport rating: #{life_support_rating}"
+end
+
+def get_oxygen_generator_rating(input_array)
+	passing_bits = []
+	bit_length = input_array[0].length
+	position = 0
+	bit_length.times do
+		if input_array.length > 1 
+			nums_in_current_position = []
+			input_array.each { |n|
+				nums_in_current_position << n[position]
+			}
+			bit_criteria = get_most(nums_in_current_position)
+			input_array.each { |n|
+				if n[position].to_s == bit_criteria.to_s && input_array.length > 0
+					passing_bits << n
+				end
+			}
+			input_array = passing_bits
+			passing_bits = []
+		end
+		position += 1
+	end	
+	return input_array[0]
+end
+
+def get_co2_scrubber_rating(input_array)
+	passing_bits = []
+	bit_length = input_array[0].length
+	position = 0
+	bit_length.times do
+		if input_array.length > 1 
+			nums_in_current_position = []
+			input_array.each { |n|
+				nums_in_current_position << n[position]
+			}
+			bit_criteria = get_least(nums_in_current_position)
+			input_array.each { |n|
+				if n[position].to_s == bit_criteria.to_s && input_array.length > 0
+					passing_bits << n
+				end
+			}
+			input_array = passing_bits
+			passing_bits = []
+		end
+		position += 1
+	end	
+	return input_array[0]
+end
+
 
 # get_power_consumption('./inputs/day3_test.txt')
-get_power_consumption('./inputs/day3_input.txt')
+# get_power_consumption('./inputs/day3_input.txt')
+# get_life_support('./inputs/day3_test.txt')
+get_life_support('./inputs/day3_input.txt')
